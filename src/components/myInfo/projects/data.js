4dcddfd740674b8abdd76e38199a47cf026fef85
nicbongo <nicbongo@protonmail.com>
2020-11-11 13:28:01 -0400

    feat: projects stylineg accented

--- src/components/myInfo/projects/data.js
+++ src/components/myInfo/projects/data.js
@@ -1,13 +1,13 @@
 import storySqyad from "../../../assets/images/Child Dash.png"
-import theQueue from "../../../assets/images/theQueue.jpg"
-import gameOfLife from "../../../assets/images/gol.jpg";
+import theQueue from "../../../assets/images/theQueue.PNG"
+import gameOfLife from "../../../assets/images/gol.PNG";
 
 export const projectData = [
   {
     name: "Story Squad - Frontend",
     pic: storySqyad,
     description: "An educational game for children focusing on creative writing and drawing submissions to compete with peers.",
-    techStack: "React, Typscript, Material-UI",
+    techStack: "React | Typscript | Material-UI",
     deploy: "https://story-squad.netlify.app/",
     repo: "https://github.com/BW-Dev-Desk-Queue-1/Front-End",
   },
@@ -15,7 +15,7 @@ export const projectData = [
     name: "Story Squad - Backend",
     pic: storySqyad,
     description: "An educational game for children focusing on creative writing and drawing submissions to compete with peers.",
-    techStack: "Typescript, TypeORM, Python",
+    techStack: "Typescript | TypeORM | Python",
     deploy: "https://story-squad.netlify.app/",
     repo: "https://github.com/BW-Dev-Desk-Queue-1/Back-End",
   },
@@ -23,7 +23,7 @@ export const projectData = [
     name: "Game of Life",
     pic: gameOfLife,
     description: "A cellular Automata app to commemorate the late J.H.Conway.",
-    techStack: "React, Material-UI, TypeScript",
+    techStack: "React | Material-UI | TypeScript",
     deploy: "https://nwk-game-of-life.now.sh/",
     repo: "https://github.com/NWKendall/game-of-life",
   },
@@ -31,7 +31,7 @@ export const projectData = [
     name: "The Queue",
     pic: theQueue,
     description: "A ticket system designed for bug tracking and technical support.",
-    techStack: "/src/assets/images/theQueue.jpg",
+    techStack: "Reac-Hook-Form | React-Redux | Redux-Logger | Axios | Thunk",
     deploy: "https://queue-5spd6wibs.now.sh/",
     repo: "https://github.com/BW-Dev-Desk-Queue-1/Front-End",
   }
--- src/components/myInfo/projects/projectCard.js
+++ src/components/myInfo/projects/projectCard.js
@@ -3,16 +3,24 @@ import "./projects.css";
 import { ReactComponent as GitHubIcon } from "../../../assets/icons/github.svg";
 
 const ProjectCard = (props) => {
-
   const { name, pic, description, techStack, deploy, repo } = props.proj;
 
   // console.log(props)
   return (
     <div className="projectCard">
-      <h4>{name}</h4>
-      <p>{techStack}</p>
-      <p>{description}</p>
-
+      <div className="blurb">
+        <a className="projTitle" href={deploy} target="_blank" rel="noreferrer">
+          <h2>{name}</h2>
+        </a>
+        <h4 className="techStack">
+          {techStack}{" "}
+          <a href={repo} target="_blank" rel="noreferrer">
+            <GitHubIcon xmlns={repo} className="projGitHubIcon"  />
+          </a>
+        </h4>
+        <p className="projectDescription">{description}</p>
+      </div>
+      <img src={pic} alt="" className="projSnapShot" />
     </div>
   );
 };
